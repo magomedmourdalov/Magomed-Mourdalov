5b0eb00f6bc191e517c08db8f888b6bc4061e9c0
Wolf Van Eeckhaute <Wolf.van.eeckhaute@outlook.com>
2019-05-29 11:33:37 +0200

    wijziging

--- Magomed Mourdalov/Form1.cs
+++ Magomed Mourdalov/Form1.cs
@@ -20,7 +20,7 @@ namespace Magomed_Mourdalov
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("bericht van wolf");
-            //dddddddddddddddddddddddddddddddddddddddd
+            //voorbeeld wijziging
         }
     }
 }
